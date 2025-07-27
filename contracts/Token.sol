abf5fcd391f0e8b232a8e115e2289a0311f0b66e
WRyder505 <wryder505@gmail.com>
2025-07-26 18:36:14 -0600

    7. Delegated Token Transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,11 +10,7 @@ contract Token {
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    event Transfer(
-        address indexed from,
-        address indexed to,
-        uint256 value
-    );
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     event Approval(
         address indexed owner,
@@ -34,15 +30,20 @@ contract Token {
     }
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value, "Token: Insufficient Funds");
-        require(_to != address(0), "Token: Recipient is address 0");
-
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-
-        emit Transfer(msg.sender, _to, _value);
+        require(balanceOf[msg.sender] >= _value, "Token: Insufficent Funds");
+        
+        _transfer(msg.sender, _to, _value);
 
         return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != address(0), "Token: Recipient is address 0");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(_from, _to, _value);
     }
 
     function approve(
@@ -50,10 +51,28 @@ contract Token {
         uint256 _value
     ) public returns (bool success) {
         require(_spender != address(0), "Token: Recipient is address 0");
-        
+
         allowance[msg.sender][_spender] = _value;
 
         emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns(bool success) {
+        require(_value <= balanceOf[_from], "Token: Insufficent Funds");
+        require(
+            _value <= allowance[_from][msg.sender],
+            "Token: Insufficient allowance"
+        );
+
+        allowance[_from][msg.sender] -= _value;
+
+        _transfer(_from, _to, _value);
+
         return true;
     }
 }
